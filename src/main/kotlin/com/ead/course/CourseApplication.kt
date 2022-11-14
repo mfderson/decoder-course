192f0801ccb23eb6d160ee94f5bac7853e411629
Anderson Freitas <mfderson@gmail.com>
2022-11-14 13:17:31 -0300

    config eureka client

--- src/main/kotlin/com/ead/course/CourseApplication.kt
+++ src/main/kotlin/com/ead/course/CourseApplication.kt
@@ -2,8 +2,10 @@ package com.ead.course
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class CourseApplication
 
 fun main(args: Array<String>) {
